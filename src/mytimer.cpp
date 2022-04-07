fa7f425e5c480f8f005be7a7e4b7a1a65baedc4c
fk1g18 <fk1g18@soton.ac.uk>
2022-04-07 17:44:06 +0300

    Added a series of prints for acc and gyro data

--- src/mytimer.cpp
+++ src/mytimer.cpp
@@ -1,24 +1,45 @@
 #include "mytimer.h"
 
-hw_timer_t * timer = NULL;
+#define WAIT_TIME 1000000;
+
+hw_timer_t *timer = NULL;
 // volatile SemaphoreHandle_t timerSemaphore;
 // portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
 bool timerEn = true;
 
-void IRAM_ATTR onTimer(){
-    // // Increment the counter and set the time of ISR
-    // portENTER_CRITICAL_ISR(&timerMux);
+void IRAM_ATTR onTimer()
+{
+  // // Increment the counter and set the time of ISR
+  // portENTER_CRITICAL_ISR(&timerMux);
 
-    //work here
+  // work here
 
+  // portEXIT_CRITICAL_ISR(&timerMux);
+  // // Give a semaphore that we can check in the loop
+  // xSemaphoreGiveFromISR(timerSemaphore, NULL);
+  // // It is safe to use digitalRead/Write here if you want to toggle an output
 
-    // portEXIT_CRITICAL_ISR(&timerMux);
-    // // Give a semaphore that we can check in the loop
-    // xSemaphoreGiveFromISR(timerSemaphore, NULL);
-    // // It is safe to use digitalRead/Write here if you want to toggle an output
+  // output the data received from the sensor pin (GYRO SENSOR)?
+  Serial.println("Printing periodic sensor data...");
+  Serial.print("Acceleration: [ X:");
+  Serial.print(mpu.getAccX());
+  Serial.print(", Y:");
+  Serial.print(mpu.getAccY());
+  Serial.print(", Z:");
+  Serial.print(mpu.getAccZ());
+  Serial.println("]");
+
+  Serial.print("Acceleration: [ X:");
+  Serial.print(mpu.getGyroX());
+  Serial.print(", Y:");
+  Serial.print(mpu.getGyroY());
+  Serial.print(", Z:");
+  Serial.print(mpu.getGyroZ());
+  Serial.println("]");
 }
 
-void timerSetup() {
+void timerSetup()
+{
   // Create semaphore to inform us when the timer has fired
   // timerSemaphore = xSemaphoreCreateBinary();
 
@@ -32,7 +53,7 @@ void timerSetup() {
 
   // Set alarm to call onTimer function every second (value in microseconds).
   // Repeat the alarm (third parameter)
-  timerAlarmWrite(timer, 1000000, true);
+  timerAlarmWrite(timer, WAIT_TIME, true);
 
   // Start an alarm
   timerAlarmEnable(timer);
